d542234749d22b464f72dfaed3320dcce6d79c61
Tuva Aarseth <tuva.emilie@hotmail.com>
2024-09-23 14:51:09 +0200

    Completed exercises

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -10,6 +10,14 @@
 //
 // TODO: write code below
 
+function numRange(lower, upper) {
+  const array = []
+  for (let i = lower; i <= upper; i++) {
+    array.push(i)
+  }
+  return array
+}
+
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -22,6 +30,14 @@
 //
 // TODO: write code below
 
+function numString(string, number) {
+  string = string.toUpperCase()
+  for (let i = 0; i < number; i++) {
+    string = string + '!'
+  }
+  return string
+}
+
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
 //
@@ -32,11 +48,44 @@
 // '11:50', 20  | '12:10'
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
-// TODO: write code below
+// TODO: write code
+
+function addTime(string, number) {
+  let hours = 0
+  let minutes = 0
+
+  if (string.length === 4) {
+    hours = parseInt(string.substr(0, 1))
+    minutes = parseInt(string.substr(2, 2))
+  } else {
+    hours = parseInt(string.substr(0, 2))
+    minutes = parseInt(string.substr(3, 2))
+  }
+
+  minutes += number
+
+  if (minutes >= 60) {
+    hours += Math.floor(minutes / 60)
+    minutes -= 60 * Math.floor(minutes / 60)
+  }
+
+  if (minutes.toString().length === 1) {
+    minutes = '0' + minutes
+  }
+
+  if (hours >= 24) {
+    hours -= 24
+    if (hours === 0) {
+      hours = '00'
+    }
+  }
+
+  return hours + ':' + minutes
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: numRange, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: numString, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: addTime // etc
 }
